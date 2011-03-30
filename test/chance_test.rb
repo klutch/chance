ebf8a65f5e4706b1d7e4d33622861188c89d0591
Graeme Collins <graeme.collins.0@gmail.com>
2011-03-30 01:19:06 -0500

    Wrote more tests

--- test/chance_test.rb
+++ test/chance_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'ruby-debug'
 
 class ChanceTest < Test::Unit::TestCase
-  def confirm_range(range, notation, iterations = 100)
+  def confirm_range(range, notation, iterations = 300)
     in_range = true
     iterations.times do
       in_range = ((range) === Chance.roll(notation))
@@ -11,11 +11,11 @@ class ChanceTest < Test::Unit::TestCase
     in_range
   end
   
-  def confirm_higher_than_lower(high_notation, low_notation, iterations = 100)
+  def confirm_higher_than_lower(high_notation, low_notation, iterations = 300)
     confirm_lower_than_higher(low_notation, high_notation)
   end
   
-  def confirm_lower_than_higher(low_notation, high_notation, iterations = 100)
+  def confirm_lower_than_higher(low_notation, high_notation, iterations = 300)
     low_total = 0
     iterations.times do
       low_total += Chance.roll(low_notation)
@@ -70,16 +70,74 @@ class ChanceTest < Test::Unit::TestCase
   end
   
   context "lowest rolls" do
-    should "average out over 100 rolls to be lower than the average high roll" do
+    should "on average be lower than the average highest roll" do
       assert_equal true, confirm_lower_than_higher("2d6v", "2d6^")
+    end
+    
+    context "lowest rolls with operations" do
+      should "on average be lower than the average highest roll with addition" do
+        assert_equal true, confirm_lower_than_higher("5d13v+10", "5d13^+10")
+      end
+
+      should "on average be lower than the average highest roll with subtraction" do
+        assert_equal true, confirm_lower_than_higher("2d23v-50", "2d23^-50")
+      end
+
+      should "on average be lower than the average highest roll with multiplication" do
+        assert_equal true, confirm_lower_than_higher("7d13v*66", "7d13^*66")
+      end
+
+      should "on average be lower than the average highest roll with division" do
+        assert_equal true, confirm_lower_than_higher("100d40v/5", "100d40^/5")
+      end
     end
   end
   
   context "highest rolls" do
-    should "average out over 100 rolls to be higher than the average low roll" do
+    should "on average be higher than the average lowest roll" do
       assert_equal true, confirm_higher_than_lower("12d60^", "12d60v")
     end
   end
   
+  context "rolls with operations" do
+    should "be in the 12 to 30 range after multiple rolls for 2d10+10" do
+      assert_equal true, confirm_range(12..30, "2d10+10")
+    end
+
+    should "be in the -6 to 14 range after multiple rolls for 4d6-10" do
+      assert_equal true, confirm_range(-6..14, "4d6-10")
+    end
+
+    should "be in the 0 to 12 range after multiple rolls for 2d60/10" do
+      assert_equal true, confirm_range(0..12, "2d60/10")
+    end
+    
+    should "be in the 100 to 2000 range after multiple rolls for d20*100" do
+      assert_equal true, confirm_range(100..2000, "d20*100")
+    end
+    
+    context "rolls with operations and a low saved roll as the offset" do
+      should "be in the 3 to 18 range for 2d6+v" do
+        assert_equal true, confirm_range(3..18, "2d6+v", 1000)
+      end
+
+      should "be in the 2 to 20 range for 3d10-v" do
+        assert_equal true, confirm_range(2..20, "3d10-v", 1000)
+      end
+      
+=begin
+      # These should fail, but don't
+      should "be in the 10 to 20 range for 10d2/v" do
+        assert_equal true, confirm_range(10..19, "10d2/v", 1000)
+      end
+
+      should "be in the 4 to 32 range for 4d2*v" do
+        assert_equal true, confirm_range(4..30, "4d2*v", 1000)
+      end
+=end
+
+    end
+    
+  end
   
 end
